491268a7a7cb30925b1225f7350aa21783e979a8
chinnikrishna33 <chinnikrishnamurthy@gmail.com>
2021-08-06 03:51:02 +1000

    Removed unused methods

--- src/main/java/com/latitude/genoapay/codingchallenge/controller/StockController.java
+++ src/main/java/com/latitude/genoapay/codingchallenge/controller/StockController.java
@@ -4,20 +4,13 @@ import com.latitude.genoapay.codingchallenge.request.StockRequest;
 import com.latitude.genoapay.codingchallenge.response.StockResponse;
 import com.latitude.genoapay.codingchallenge.service.StockService;
 
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 @RestController
 @RequestMapping("/stock")
@@ -43,48 +36,6 @@ public class StockController {
         }
 
         return ResponseEntity.ok(response);
-    }
-
-    @GetMapping(value = "/maxProfit2", produces = {"application/json"}, consumes = "application/json")
-    public ResponseEntity<Object> getMaxProfit(@RequestParam("identifier") String identifier,
-            @RequestParam("startDateTime") String startDateTime, @RequestParam("endDateTime") String endDateTime,
-            @RequestParam("stockPrices") String stockPrices){
-
-//        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-        String[] strArr = stockPrices.split(",");
-        int[] stockPricesArr = new int[strArr.length];
-        for (int i = 0; i < strArr.length; i++) {
-             stockPricesArr[i] = Integer.parseInt(strArr[i]);
-
-        }
-        StockRequest request = new StockRequest(identifier, (new DateTime( startDateTime)).toDate(), (new DateTime( endDateTime)).toDate(), stockPrices);
-
-//        try {
-//            request = new StockRequest(identifier, (new DateTime( startDateTime)).toDate(), (new DateTime( endDateTime)).toDate(), stockPricesArr);
-//        } catch (ParseException e) {
-//            throw new ParseException("Start/End time format is incorrect", e.getErrorOffset());
-//        }
-
-        if (!stockService.isValidRequest(request)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: Invalid Request - Missing parameters");
-        }
-
-        StockResponse response;
-        try {
-            response = stockService.getMaximumProfit(request);
-            if (response.getMaxProfit() == 0) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: Cannot make any Profit within the Start and End Times");
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
-
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
     }
 
 }
